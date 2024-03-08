2cc968bf1a1af8a0500ba5a31094509bcf374c84
Mustafa Masri <mustafamasry180@gmail.com>
2024-03-08 15:48:55 +0300

    Add project files.

---
+++ some cool homework/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace some_cool_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            openFileToolStripMenuItem = new ToolStripMenuItem();
+            saveFileToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            cutToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            pasteToolStripMenuItem = new ToolStripMenuItem();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            textColorToolStripMenuItem = new ToolStripMenuItem();
+            backgroundColorToolStripMenuItem = new ToolStripMenuItem();
+            fontToolStripMenuItem = new ToolStripMenuItem();
+            templatesToolStripMenuItem = new ToolStripMenuItem();
+            cToolStripMenuItem = new ToolStripMenuItem();
+            cToolStripMenuItem1 = new ToolStripMenuItem();
+            cToolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            reportAProblemToolStripMenuItem = new ToolStripMenuItem();
+            suggestAFeatureToolStripMenuItem = new ToolStripMenuItem();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            richTextBox1 = new RichTextBox();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            colorDialog1 = new ColorDialog();
+            fontDialog1 = new FontDialog();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, settingsToolStripMenuItem, templatesToolStripMenuItem, toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openFileToolStripMenuItem, saveFileToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(155, 26);
+            newToolStripMenuItem.Text = "New";
+            newToolStripMenuItem.Click += newToolStripMenuItem_Click;
+            // 
+            // openFileToolStripMenuItem
+            // 
+            openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            openFileToolStripMenuItem.Size = new Size(155, 26);
+            openFileToolStripMenuItem.Text = "Open File";
+            openFileToolStripMenuItem.Click += openFileToolStripMenuItem_Click;
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            saveFileToolStripMenuItem.Size = new Size(155, 26);
+            saveFileToolStripMenuItem.Text = "Save File";
+            saveFileToolStripMenuItem.Click += saveFileToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(155, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cutToolStripMenuItem, copyToolStripMenuItem, pasteToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(104, 24);
+            editToolStripMenuItem.Text = "Text options";
+            // 
+            // cutToolStripMenuItem
+            // 
+            cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            cutToolStripMenuItem.Size = new Size(126, 26);
+            cutToolStripMenuItem.Text = "Cut";
+            cutToolStripMenuItem.Click += cutToolStripMenuItem_Click;
+            // 
+            // copyToolStripMenuItem
+            // 
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            copyToolStripMenuItem.Size = new Size(126, 26);
+            copyToolStripMenuItem.Text = "Copy";
+            copyToolStripMenuItem.Click += copyToolStripMenuItem_Click;
+            // 
+            // pasteToolStripMenuItem
+            // 
+            pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            pasteToolStripMenuItem.Size = new Size(126, 26);
+            pasteToolStripMenuItem.Text = "Paste";
+            pasteToolStripMenuItem.Click += pasteToolStripMenuItem_Click;
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { colorToolStripMenuItem, fontToolStripMenuItem });
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(70, 24);
+            settingsToolStripMenuItem.Text = "Format";
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { textColorToolStripMenuItem, backgroundColorToolStripMenuItem });
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(128, 26);
+            colorToolStripMenuItem.Text = "Color";
+            // 
+            // textColorToolStripMenuItem
+            // 
+            textColorToolStripMenuItem.Name = "textColorToolStripMenuItem";
+            textColorToolStripMenuItem.Size = new Size(211, 26);
+            textColorToolStripMenuItem.Text = "Text Color";
+            textColorToolStripMenuItem.Click += textColorToolStripMenuItem_Click;
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            backgroundColorToolStripMenuItem.Size = new Size(211, 26);
+            backgroundColorToolStripMenuItem.Text = "Background Color";
+            backgroundColorToolStripMenuItem.Click += backgroundColorToolStripMenuItem_Click;
+            // 
+            // fontToolStripMenuItem
+            // 
+            fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            fontToolStripMenuItem.Size = new Size(128, 26);
+            fontToolStripMenuItem.Text = "Font";
+            fontToolStripMenuItem.Click += fontToolStripMenuItem_Click;
+            // 
+            // templatesToolStripMenuItem
+            // 
+            templatesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cToolStripMenuItem, cToolStripMenuItem1, cToolStripMenuItem2 });
+            templatesToolStripMenuItem.Name = "templatesToolStripMenuItem";
+            templatesToolStripMenuItem.Size = new Size(91, 24);
+            templatesToolStripMenuItem.Text = "Templates";
+            // 
+            // cToolStripMenuItem
+            // 
+            cToolStripMenuItem.Name = "cToolStripMenuItem";
+            cToolStripMenuItem.Size = new Size(121, 26);
+            cToolStripMenuItem.Text = "C";
+            cToolStripMenuItem.Click += cToolStripMenuItem_Click;
+            // 
+            // cToolStripMenuItem1
+            // 
+            cToolStripMenuItem1.Name = "cToolStripMenuItem1";
+            cToolStripMenuItem1.Size = new Size(121, 26);
+            cToolStripMenuItem1.Text = "C#";
+            cToolStripMenuItem1.Click += cToolStripMenuItem1_Click;
+            // 
+            // cToolStripMenuItem2
+            // 
+            cToolStripMenuItem2.Name = "cToolStripMenuItem2";
+            cToolStripMenuItem2.Size = new Size(121, 26);
+            cToolStripMenuItem2.Text = "C++";
+            cToolStripMenuItem2.Click += cToolStripMenuItem2_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem, reportAProblemToolStripMenuItem, suggestAFeatureToolStripMenuItem });
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(55, 24);
+            toolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(224, 26);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // reportAProblemToolStripMenuItem
+            // 
+            reportAProblemToolStripMenuItem.Name = "reportAProblemToolStripMenuItem";
+            reportAProblemToolStripMenuItem.Size = new Size(224, 26);
+            reportAProblemToolStripMenuItem.Text = "Report A Problem...";
+            reportAProblemToolStripMenuItem.Click += reportAProblemToolStripMenuItem_Click;
+            // 
+            // suggestAFeatureToolStripMenuItem
+            // 
+            suggestAFeatureToolStripMenuItem.Name = "suggestAFeatureToolStripMenuItem";
+            suggestAFeatureToolStripMenuItem.Size = new Size(224, 26);
+            suggestAFeatureToolStripMenuItem.Text = "Suggest A Feature...";
+            suggestAFeatureToolStripMenuItem.Click += suggestAFeatureToolStripMenuItem_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 31);
+            button1.Name = "button1";
+            button1.Size = new Size(53, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Cut";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(71, 31);
+            button2.Name = "button2";
+            button2.Size = new Size(64, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Copy";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(141, 31);
+            button3.Name = "button3";
+            button3.Size = new Size(62, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Paste";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(10, 78);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(778, 360);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.Filter = "Text File (.txt)|*.txt";
+            saveFileDialog1.FileOk += saveFileDialog1_FileOk;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "TextifyMyText";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem openFileToolStripMenuItem;
+        private ToolStripMenuItem saveFileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem pasteToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripMenuItem textColorToolStripMenuItem;
+        private ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private ToolStripMenuItem fontToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private RichTextBox richTextBox1;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private ColorDialog colorDialog1;
+        private FontDialog fontDialog1;
+        private ToolStripMenuItem templatesToolStripMenuItem;
+        private ToolStripMenuItem cToolStripMenuItem;
+        private ToolStripMenuItem cToolStripMenuItem1;
+        private ToolStripMenuItem cToolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem reportAProblemToolStripMenuItem;
+        private ToolStripMenuItem suggestAFeatureToolStripMenuItem;
+    }
+}
---
+++ some cool homework/Form1.cs
@@ -0,0 +1,188 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2023-2024 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 1
+**				ÖĞRENCİ ADI............: Mustafa Masri
+**				ÖĞRENCİ NUMARASI.......: G221210580
+**                         DERSİN ALINDIĞI GRUP...: 2. Öğretim A Grubu
+****************************************************************************/
+
+//hello user, this program was made for my object oriented class homework.
+//I will probably never check up on it again, so good luck with it!
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace some_cool_homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region file options
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if the user did not save the file, the program will ask the user if they want to save the file
+            //it will save as a text file
+            if (richTextBox1.Text != "")
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save the file?", "Save", MessageBoxButtons.YesNoCancel);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveFileDialog1.ShowDialog();
+                    richTextBox1.Clear();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    richTextBox1.Clear();
+                }
+            }
+            else
+            {
+                richTextBox1.Clear();
+            }
+        }
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //save the file
+            richTextBox1.SaveFile(saveFileDialog1.FileName);
+
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //when the user clicks, the save file dialog will open
+            saveFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //when the user clicks, the open file dialog will open, and the user can choose a text file
+            openFileDialog1.ShowDialog();
+            //and the thing will take the text from the file and paste it into the rich text box
+            richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //before exiting, the program will ask the user if they want to save the file
+            if (richTextBox1.Text != "")
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save the file?", "Save", MessageBoxButtons.YesNoCancel);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveFileDialog1.ShowDialog();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        #endregion
+        #region Cut copy paste
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+        #endregion
+        #region text options
+        private void textColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //when the user clicks, the color dialog will open, and the user can choose a color
+            colorDialog1.ShowDialog();
+            richTextBox1.SelectionColor = colorDialog1.Color;
+
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //the background of the text will change to the color the user chooses
+            colorDialog1.ShowDialog();
+            richTextBox1.SelectionBackColor = colorDialog1.Color;
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //the font dialog will open, and the user can choose the font
+            fontDialog1.ShowDialog();
+            richTextBox1.SelectionFont = fontDialog1.Font;
+        }
+        #endregion
+        #region templates
+        private void cToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //when the user clicks this, a ready template for the language C will appear, and the user will start coding
+            richTextBox1.Text = "#include <stdio.h>\nint main()\n{\n\nreturn 0;\n}";
+
+
+        }
+
+        private void cToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //same thing as the previous one but for C#
+            richTextBox1.Text = "using System;\nclass Program\n{\nstatic void Main()\n{\n\n}\n}";
+        }
+
+        private void cToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            //same thing but for C++
+            richTextBox1.Text = "#include <iostream>\nusing namespace std;\nint main()\n{\n\nreturn 0;\n}";
+        }
+        #endregion
+        #region about
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //when the user clicks this, a message box will appear with the information about the program
+            MessageBox.Show("Greetings, user!\nthis is a simple program that I made for some homework, and I probably will never come back to check up on it. Good luck!");
+        }
+
+        private void reportAProblemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Woah, it seems like you have a problem. Well, I'm too lazy to go fix it, so you'll have to deal with it yourself. Sorry!");
+        }
+
+        private void suggestAFeatureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Looks like you want to suggest a feature. Well, I'm too lazy to add it, so you'll have to deal with it yourself. Sorry!");
+        }
+        #endregion
+    }
+}
